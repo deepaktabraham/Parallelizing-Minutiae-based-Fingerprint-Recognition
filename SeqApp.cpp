7701bfde2f0b133ef6eeb49a7dc5290d44db1983
deepaktabraham <deepaktabraham@gmail.com>
2017-03-09 13:40:48 -0500

    obtain orientation

--- SeqApp.cpp
+++ SeqApp.cpp
@@ -3,19 +3,26 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 
+
 #define PI 3.14159265
+
 
 using namespace std;
 using namespace cv;
+
+
 int main()
 {
 	Mat img, img_db;
+
 	
 	// read fingerprint image
 	img = imread("f0001_01.png", CV_LOAD_IMAGE_GRAYSCALE);
+
 	
 	// convert image to double precision
 	img.convertTo(img_db, CV_64FC1);
+
 
 	// initialize matrices for calculation of pixel orientation
 	Mat Gx(img_db.cols, img_db.rows, CV_64FC1);
@@ -24,31 +31,30 @@ int main()
 	Mat Vy(img_db.cols, img_db.rows, CV_64FC1);
 	Mat orientation(img_db.cols, img_db.rows, CV_64FC1);
 
+
 	// computing image gradient with Sobel operator
 	Sobel(img_db, Gx, CV_64F, 1, 0, 3);
 	Sobel(img_db, Gy, CV_64F, 0, 1, 3);
+
 	
 	// determine pixel orientation, adjust it to [0, 180] degree range
 	Vx = 2 * Gx.mul(Gy);
 	Vy = Gx.mul(Gx) - Gy.mul(Gy);
-	
+
 	for(int i = 0; i < Gx.rows; i++)
 		for(int j = 0; j < Gx.cols; j++)
-			orientation.at<double>(i, j) = 90 + atan2(Vx.at<double>(i, j),Vy.at<double>(i, j)) * 0.5 * 180 / PI ;
+			orientation.at<double>(i, j) = 90 + atan2(Vx.at<double>(i, j),\
+					Vy.at<double>(i, j)) * 0.5 * 180 / PI ;
 
-
-	cout<<orientation.at<double>(100, 100)<<"\n"; 
 #if 0
 	// write:
-FileStorage fs("myfile.txt",FileStorage::WRITE);
-fs << "mat1" << orientation;
+	FileStorage fs("f0001_01.xml", FileStorage::WRITE);
+	fs << "f0001_01" << orientation;
 
-// read:
-FileStorage fs("myfile.txt",FileStorage::READ);
-fs["mat1"] >> m;
+	// read:
+	FileStorage fs("f0001_01.xml", FileStorage::READ);
+	fs["f0001_01"] >> new_orientation;
 #endif
-
-
 
 	imshow("fingerprint", img);
 	waitKey(0);
