f35fc8f495cf4f918bbc5174f701a117c06aaead
deepaktabraham <deepaktabraham@gmail.com>
2017-03-09 13:25:34 -0500

    obtain orientation

--- SeqApp.cpp
+++ SeqApp.cpp
@@ -1,22 +1,59 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/viz/vizcore.hpp>
+#include <iostream>
+#include <cmath>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
+#define PI 3.14159265
+
+using namespace std;
+using namespace cv;
 int main()
 {
-    cv::viz::Viz3d window = cv::viz::Viz3d("Viz demonstration");
+	int i, j;
+	Mat img, img_db;
+	
+	// read fingerprint image
+	img = imread("f0001_01.png", CV_LOAD_IMAGE_GRAYSCALE);
+	
+	// convert image to double precision
+	img.convertTo(img_db, CV_64FC1);
 
-    cv::Point3d min(0.25, 0.0, 0.25);
-    cv::Point3d max(0.75, 0.5, 0.75);
+	// initialize matrices for calculation of pixel orientation
+	Mat Gx(img_db.cols, img_db.rows, CV_64FC1);
+	Mat Gy(img_db.cols, img_db.rows, CV_64FC1);
+	Mat Vx(img_db.cols, img_db.rows, CV_64FC1);
+	Mat Vy(img_db.cols, img_db.rows, CV_64FC1);
+	Mat orientation(img_db.cols, img_db.rows, CV_64FC1);
 
-    cv::viz::WCube cube(min, max, true, cv::viz::Color::blue());
-    cube.setRenderingProperty(cv::viz::LINE_WIDTH, 4.0);
+	// computing image gradient with Sobel operator
+	Sobel(img_db, Gx, CV_64F, 1, 0, 3);
+	Sobel(img_db, Gy, CV_64F, 0, 1, 3);
+	
+	// determine pixel orientation, adjust it to [0, 180] degree range
+	Vx = 2*Gx.mul(Gy);
+	Vy = Gx.mul(Gx) - Gy.mul(Gy);
+	for(int i = 0; i < Gx.rows; i++)
+		for(int j= 0; j< Gx.cols; j++)
+		{
+			orientation.at<double>(i,j) = 90 + atan2(Vx.at<double>(i,j),Vy.at<double>(i,j)) * 0.5 * 180/PI ;
+		}
 
-    window.showWidget("Axis widget", cv::viz::WCoordinateSystem());
-    window.showWidget("Cube widget", cube);
+#if 0
+	// write:
+FileStorage fs("myfile.txt",FileStorage::WRITE);
+fs << "mat1" << orientation;
 
-    while(!window.wasStopped()) {
-        window.spinOnce(1, true);
-    }
+// read:
+FileStorage fs("myfile.txt",FileStorage::READ);
+fs["mat1"] >> m;
+#endif
 
-    return 0;
+
+
+	imshow("fingerprint",img);
+	waitKey(0);
+	
+
+	return 0;
 }
+
